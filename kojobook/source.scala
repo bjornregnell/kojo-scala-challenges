0582be082206c5b1848bcf732be77d46883c9b42
bjornregnell <bjorn.regnell@cs.lth.se>
2015-02-03 18:54:48 +0100

    add challenge kvadrat

--- kojobook/source.scala
+++ kojobook/source.scala
@@ -1,6 +1,7 @@
-println("*** Running script: kojobook/source.scala")
-
+//scala script to generate book body from source
 import scaboo._
+
+println("\n\n*** scaboo: Running script: kojobook/source.scala ***")
 
 val chapters = Seq(
   Challenge(id="fram", head="Ditt första program", template=TextWithImage("fram.png"), 
@@ -12,6 +13,17 @@ sudda
 fram
        """.trim, size=24),
       Para("Tryck på den gröna play-knappen","för att köra igång ditt program.")
+    )
+  ),
+  Challenge(id="kvadrat", head="Rita en kvadrat", template=TextWithImage("kvadrat.png"), 
+    goals=Seq(Para("Att satser i sekvens körs i tur och ordning.","Att ordningen spelar stor roll.")),
+    task=Seq(
+      Para("Skriv in och utöka detta program så att paddan ritar en kvadrat:"),
+      Code("""
+sudda
+fram
+höger
+       """.trim, size=24)
     )
   )
 )
